40f3348696bb88ef05b3eab0f3ac1a6ede9a0e95
dishangti <16698219+dishangti@users.noreply.github.com>
2023-08-02 20:19:46 +0800

    Code for visional image comparison.

---
+++ imgprocess/optDiff.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+img1 = cv2.imread("./Sam_vgg/5463.jpg",0)
+img2 = cv2.imread("./Sam_vgg_ft/5463.jpg",0)
+
+img = np.abs(img1 - img2)
+max_pix = np.max(img)
+min_pix = np.min(img)
+
+img.astype(np.float64)
+
+img = (img - min_pix) / (max_pix - min_pix) * 255
+img = 15*np.sqrt(img)
+
+img.astype(np.uint8)
+
+cv2.imwrite('5463.png', 255-  img)
+
+#6518
+#1689
