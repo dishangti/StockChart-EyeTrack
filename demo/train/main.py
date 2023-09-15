6248c91ff30505f65594801ed8e9500bef8d47c8
dishangti <16698219+dishangti@users.noreply.github.com>
2023-09-15 16:31:49 +0800

    Show a chart and draw on it.

--- demo/train/main.py
+++ demo/train/main.py
@@ -1,20 +1,88 @@
-from PyQt6.QtWidgets import QMainWindow, QMessageBox
+import typing
+from PyQt6.QtWidgets import QGraphicsSceneMouseEvent, QMainWindow, QMessageBox
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QImage, QPixmap
-from PyQt6.QtWidgets import QGraphicsPixmapItem, QGraphicsScene
+from PyQt6.QtWidgets import QGraphicsPixmapItem, QGraphicsScene, QGraphicsPathItem
+from PyQt6.QtGui import QPainterPath, QPen
+from PyQt6 import QtCore
 from window import Ui_MainWindow
 import sys
+
+class DrawScene(QGraphicsScene):
+    def __init__(self, width, height):
+        super(DrawScene, self).__init__()
+        self.setSceneRect(0,0,width,height)
+
+    def mousePressEvent(self, event: QGraphicsSceneMouseEvent | None) -> None:
+        if event.button() == QtCore.Qt.MouseButton.LeftButton:
+            self.QGraphicsPath = QGraphicsPathItem()
+            self.path1 = QPainterPath()
+            self.path1.moveTo(event.scenePos())         # path begin
+            pp = QPen()
+            pp.setColor(QtCore.Qt.GlobalColor.white)    # set a pen
+            pp.setWidth(10)
+            self.QGraphicsPath.setPen(pp)
+            self.addItem(self.QGraphicsPath)
+        return super().mousePressEvent(event)
+
+    def mouseMoveEvent(self, event: QGraphicsSceneMouseEvent | None) -> None:
+        if event.buttons() & QtCore.Qt.MouseButton.LeftButton:
+            if self.path1:
+                self.path1.lineTo(event.scenePos()) # Move and link points
+                self.QGraphicsPath.setPath(self.path1)
+        return super().mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event: QGraphicsSceneMouseEvent | None) -> None:
+        if event.button() == QtCore.Qt.MouseButton.LeftButton:
+            if self.path1:
+                self.path1.closeSubpath()   # End a path
+        return super().mouseReleaseEvent(event)
 
 class mainWin(QMainWindow, Ui_MainWindow):
     def __init__(self,parent=None):
         super(mainWin, self).__init__()
         self.setupUi(self)
+
+        self.setFixedSize(self.width(), self.height())  # Fix window size
+        self.setWindowFlags(QtCore.Qt.WindowType.WindowCloseButtonHint | QtCore.Qt.WindowType.WindowMinimizeButtonHint)
+
+        ### Show original image
+        self._showImage('./116_origin.png')
+
+        self.isOrigin = True
     
-    def evaluate():
+    def _showImage(self, imgPath):
+        """
+        Image should be scaled as 16:9
+        """
+        frame = QImage(imgPath)
+        rcontent = self.graphicsView.contentsRect()
+        graphicWidth = rcontent.width()
+        graphicHeight = rcontent.height()
+        scaled = frame.scaled(graphicWidth, graphicHeight, QtCore.Qt.AspectRatioMode.IgnoreAspectRatio, QtCore.Qt.TransformationMode.SmoothTransformation)
+        pix = QPixmap.fromImage(scaled)
+        item = QGraphicsPixmapItem(pix)
+        scene  = DrawScene(graphicWidth,graphicHeight)
+        scene.addItem(item)
+        self.graphicsView.setScene(scene)
+
+    def evaluate(self):
         pass
 
-    def answer():
-        pass
+    def answer(self):
+        if self.isOrigin:
+            self._showImage('./116_ans.png')
+            self.isOrigin = False
+            self.pushButton_evaluate.setEnabled(False)
+        else:
+            self._showImage('./116_origin')
+            self.isOrigin = True
+            self.pushButton_evaluate.setEnabled(True)
+
+    def reset(self):
+        self._showImage('./116_origin')
+        self.isOrigin = True
+        self.pushButton_evaluate.setEnabled(True)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
