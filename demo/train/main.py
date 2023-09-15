1bf37b8e47ea7ec5c447f9ff816a6b861bf0987b
dishangti <16698219+dishangti@users.noreply.github.com>
2023-09-15 17:28:38 +0800

    Save what user input.

--- demo/train/main.py
+++ demo/train/main.py
@@ -3,39 +3,56 @@ from PyQt6.QtWidgets import QGraphicsSceneMouseEvent, QMainWindow, QMessageBox
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QImage, QPixmap
 from PyQt6.QtWidgets import QGraphicsPixmapItem, QGraphicsScene, QGraphicsPathItem
-from PyQt6.QtGui import QPainterPath, QPen
+from PyQt6.QtGui import QPainterPath, QPen, QPainter
 from PyQt6 import QtCore
 from window import Ui_MainWindow
 import sys
 
 class DrawScene(QGraphicsScene):
-    def __init__(self, width, height):
+    def __init__(self, width, height, child=None):
         super(DrawScene, self).__init__()
         self.setSceneRect(0,0,width,height)
+        if child:
+            child.setSceneRect(0,0,width,height)
+        self.child = child
 
     def mousePressEvent(self, event: QGraphicsSceneMouseEvent | None) -> None:
         if event.button() == QtCore.Qt.MouseButton.LeftButton:
-            self.QGraphicsPath = QGraphicsPathItem()
+            self.QGraphicsPath1 = QGraphicsPathItem()
+            self.QGraphicsPath2 = QGraphicsPathItem()
             self.path1 = QPainterPath()
+            self.path2 = QPainterPath()
             self.path1.moveTo(event.scenePos())         # path begin
-            pp = QPen()
-            pp.setColor(QtCore.Qt.GlobalColor.white)    # set a pen
-            pp.setWidth(10)
-            self.QGraphicsPath.setPen(pp)
-            self.addItem(self.QGraphicsPath)
+            self.path2.moveTo(event.scenePos())
+            pp1 = QPen()
+            pp1.setColor(QtCore.Qt.GlobalColor.white)    # set a pen
+            pp1.setWidth(10)
+            pp2 = QPen()
+            pp2.setColor(QtCore.Qt.GlobalColor.white)
+            pp2.setWidth(10)
+            self.QGraphicsPath1.setPen(pp1)
+            self.QGraphicsPath2.setPen(pp2)
+            self.addItem(self.QGraphicsPath1)
+            if self.child:
+                self.child.addItem(self.QGraphicsPath2)
         return super().mousePressEvent(event)
 
     def mouseMoveEvent(self, event: QGraphicsSceneMouseEvent | None) -> None:
         if event.buttons() & QtCore.Qt.MouseButton.LeftButton:
             if self.path1:
                 self.path1.lineTo(event.scenePos()) # Move and link points
-                self.QGraphicsPath.setPath(self.path1)
+                self.QGraphicsPath1.setPath(self.path1)
+            if self.path2:
+                self.path2.lineTo(event.scenePos())
+                self.QGraphicsPath2.setPath(self.path2)
         return super().mouseMoveEvent(event)
 
     def mouseReleaseEvent(self, event: QGraphicsSceneMouseEvent | None) -> None:
         if event.button() == QtCore.Qt.MouseButton.LeftButton:
             if self.path1:
                 self.path1.closeSubpath()   # End a path
+            if self.path2:
+                self.path2.closeSubpath()
         return super().mouseReleaseEvent(event)
 
 class mainWin(QMainWindow, Ui_MainWindow):
@@ -47,11 +64,11 @@ class mainWin(QMainWindow, Ui_MainWindow):
         self.setWindowFlags(QtCore.Qt.WindowType.WindowCloseButtonHint | QtCore.Qt.WindowType.WindowMinimizeButtonHint)
 
         ### Show original image
-        self.drawscene = self._showImage('./116_origin.png', True)
+        self.drawscene = self._showOrigin('./116_origin.png')
 
         self.isOrigin = True
-    
-    def _showImage(self, imgPath, drawable):
+
+    def _showOrigin(self, imgPath):
         """
         Image should be scaled as 16:9
         """
@@ -62,21 +79,42 @@ class mainWin(QMainWindow, Ui_MainWindow):
         scaled = frame.scaled(graphicWidth, graphicHeight, QtCore.Qt.AspectRatioMode.IgnoreAspectRatio, QtCore.Qt.TransformationMode.SmoothTransformation)
         pix = QPixmap.fromImage(scaled)
         item = QGraphicsPixmapItem(pix)
-        if drawable:
-            scene  = DrawScene(graphicWidth,graphicHeight)
-        else:
-            scene = QGraphicsScene()
-            scene.setSceneRect(0,0,graphicWidth,graphicHeight)
+        child = DrawScene(graphicWidth,graphicHeight)
+        scene  = DrawScene(graphicWidth,graphicHeight, child)
+        scene.addItem(item)
+        self.graphicsView.setScene(scene)
+        return scene
+
+    def _showImage(self, imgPath):
+        """
+        Image should be scaled as 16:9
+        """
+        frame = QImage(imgPath)
+        rcontent = self.graphicsView.contentsRect()
+        graphicWidth = rcontent.width()
+        graphicHeight = rcontent.height()
+        scaled = frame.scaled(graphicWidth, graphicHeight, QtCore.Qt.AspectRatioMode.IgnoreAspectRatio, QtCore.Qt.TransformationMode.SmoothTransformation)
+        pix = QPixmap.fromImage(scaled)
+        item = QGraphicsPixmapItem(pix)
+        scene = QGraphicsScene()
+        scene.setSceneRect(0,0,graphicWidth,graphicHeight)
         scene.addItem(item)
         self.graphicsView.setScene(scene)
         return scene
 
     def evaluate(self):
-        pass
+        self.drawscene.child.clearSelection()
+        image = QImage(self.drawscene.child.sceneRect().size().toSize(), QImage.Format.Format_ARGB32)
+        image.fill(QtCore.Qt.GlobalColor.black)
+
+        painter = QPainter(image)
+        self.drawscene.child.render(painter)
+        image.save('./116_user.png')
+        del painter
 
     def answer(self):
         if self.isOrigin:
-            self._showImage('./116_ans.png', False)
+            self._showImage('./116_ans.png')
             self.isOrigin = False
             self.pushButton_evaluate.setEnabled(False)
         else:
@@ -85,7 +123,7 @@ class mainWin(QMainWindow, Ui_MainWindow):
             self.pushButton_evaluate.setEnabled(True)
 
     def reset(self):
-        self.drawscene = self._showImage('./116_origin')
+        self.drawscene = self._showOrigin('./116_origin')
         self.isOrigin = True
         self.pushButton_evaluate.setEnabled(True)
 
